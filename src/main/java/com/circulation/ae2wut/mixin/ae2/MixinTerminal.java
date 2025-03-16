e52c150e9aa519441611ad47f8d6d5bf63ab647d
Circulate233 <a18966009058@163.com>
2025-03-16 15:36:58 +0800

    补充一条对无线接口终端的快捷键修复....太神秘了AE2UEL

---
+++ src/main/java/com/circulation/ae2wut/mixin/ae2/MixinTerminal.java
@@ -0,0 +1,29 @@
+package com.circulation.ae2wut.mixin.ae2;
+
+import appeng.api.definitions.IItemDefinition;
+import appeng.core.sync.GuiBridge;
+import appeng.items.tools.powered.Terminal;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Terminal.class,remap = false)
+public class MixinTerminal {
+
+    @Final
+    @Shadow
+    GuiBridge bridge;
+    @Final
+    @Shadow
+    IItemDefinition itemDefinition;
+
+    @Inject(method = "getBridge",at = @At("RETURN"), cancellable = true)
+    public void getBridgeMixin(CallbackInfoReturnable<GuiBridge> cir) {
+        if (this.bridge == GuiBridge.GUI_INTERFACE_TERMINAL){
+            cir.setReturnValue(GuiBridge.GUI_WIRELESS_INTERFACE_TERMINAL);
+        }
+    }
+}
